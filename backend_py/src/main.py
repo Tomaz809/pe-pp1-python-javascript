a1fa622b0fca3d6691e879966d39e9b90c0e7abb
lautaro <85296737+skidlauti@users.noreply.github.com>
2026-06-22 21:35:40 -0300

    Update main.py

--- backend_py/src/main.py
+++ backend_py/src/main.py
@@ -1,1 +1,18 @@
+from fastapi import FastAPI 
+from fastapi.middleware.cors import CORSMiddleware
 
+from routers.articulos import articulos_routers 
+
+app = FASTAPI(
+title="API de Farmacia",
+)
+
+app.include_router(articulos_routers, prefix="/articulos", tags=["Articulos"])
+
+app.add_middleware(
+        CORSMiddleware,
+        allow_origins=["http://127.0.0.1:5500"],
+        allow_credentials=True,
+        allow_methods=["*"],
+        allow_headers=["*"],
+)
